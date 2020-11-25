6d768cce073526a1cadf541005da81dd1ad37387
khanhvo99 <khanhvo14031999@gmail.com>
2020-11-25 23:07:56 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-100days-day-7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-100days-day-7)
---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,72 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from "@angular/core";
+
+@Component({
+  selector: `progress-app`,
+  template: `
+    <div
+      class="progress-bar-container"
+      [style.backgroundColor]="backgroundColor"
+    >
+      <div
+        class="progress"
+        [style.backgroundColor]="progressColor"
+        [style.width]="progress + '%'"
+      ></div>
+    </div>
+  `,
+  styles: [
+    `
+      .progress-bar-container,
+      .progress {
+        height: 20px;
+      }
+      .progress-bar-container {
+        width: 100%;
+      }
+    `
+  ]
+})
+export class ProgressBarComponent implements OnInit, OnChanges {
+  @Input() set progress(val: number) {
+    if (typeof val !== "number") {
+      const progress = Number(val);
+      if (Number.isNaN(progress)) {
+        this._progress = 0;
+      } else {
+        this._progress = progress;
+      }
+    } else {
+      this._progress = val;
+    }
+    this._progress = val;
+  }
+  private _progress = 50;
+  get progress() {
+    return this._progress;
+  }
+
+  @Input() backgroundColor = "yellow";
+  @Input() progressColor = "blue";
+
+  constructor() {}
+  ngOnInit(): void {
+    // console.log("init", {
+    //   progress: this.progress,
+    //   backgroundColor: this.backgroundColor,
+    //   progressColor: this.progressColor
+    // });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log("change", {
+    //   progress: this.progress,
+    //   backgroundColor: this.backgroundColor,
+    //   progressColor: this.progressColor
+    // });
+  }
+}
